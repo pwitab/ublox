8837c63c70e03f67f5b467cf485c386380a66166
Henrik Palmlund Wahlgren <henrik@pwit.se>
2018-10-24 11:24:50 +0200

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name='ublox',
-    version='0.0.2', python_requires='~=3.6',
+    version='0.1.0', python_requires='~=3.6',
     description="Python library for U-blox cellular modules.",
     long_description=readme + '\n\n' + history,
     author="Henrik Palmlund Wahlgren @ Palmlund Wahlgren Innovative Technology AB",
