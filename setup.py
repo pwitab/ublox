44296e9725be5c66a673d07aa92ac22a383bd85d
Henrik Palmlund Wahlgren <henrik@pwit.se>
2018-11-22 10:32:10 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name=NAME,
-    version='0.1.1',
+    version='0.1.2',
     python_requires=REQUIRES_PYTHON,
     description=DESCRIPTION,
     long_description=readme + '\n\n' + history,
